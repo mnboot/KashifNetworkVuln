4ee4edf15b5afa917239e24f7f652c48e73002bc
Mnboot <ab0000d06@hotmail.com>
2026-04-18 16:58:52 +0300

    test

--- src/main/java/com/forgepacket/kashifVuln/Main.java
+++ src/main/java/com/forgepacket/kashifVuln/Main.java
@@ -1,9 +1,10 @@
 package com.forgepacket.kashifVuln;
+
 import com.forgepacket.kashifVuln.GUI.Launcher;
 
 public class Main {
-    public static void main(String[] args) {
+    static void main(String[] args) {
         Launcher.main(args);
-        System.out.println("Tes");
+
     }
 }
