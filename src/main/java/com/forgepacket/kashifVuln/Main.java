22eb4a890d2047fd608dabe536ae5839f200b947
Mnboot <ab0000d06@hotmail.com>
2026-04-18 17:01:11 +0300

    d

--- src/main/java/com/forgepacket/kashifVuln/Main.java
+++ src/main/java/com/forgepacket/kashifVuln/Main.java
@@ -1,10 +1,9 @@
 package com.forgepacket.kashifVuln;
-
 import com.forgepacket.kashifVuln.GUI.Launcher;
 
 public class Main {
     static void main(String[] args) {
         Launcher.main(args);
-
+        System.out.println("Tes");
     }
 }
--- src/test/java/TestCalc.java
+++ src/test/java/TestCalc.java
@@ -1,7 +1,9 @@
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
 class Calc{
     public int add(int a, int b){
-        return a - b;
+        return a + b;
     }
 }
 
@@ -10,6 +12,6 @@ public class TestCalc {
     @Test
     public void testAddition(){
         Calc calc = new Calc();
-
+        Assertions.assertEquals(3, calc.add(1, 2));
     }
 }
